0230ca791adf1d380b58a314cc737ad8811d8774
NikolaPeric286 <nikolaperic286@gmail.com>
2025-10-03 00:10:38 -0400

    cross platform support changes

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # drinkDB
 
 
+windows compilation instructions
+
+git clone https://github.com/NikolaPeric286/drinkDB
+cd drinkDB
+git submodule update --init --recursive
+cd build
+cmake .. -G Ninja
+ninja 
+
+
+Linux
 export DISPLAY=$(grep -m1 nameserver /etc/resolv.conf | awk '{print $2}'):0
 export LIBGL_ALWAYS_INDIRECT=1
--- src/gui/App.cpp
+++ src/gui/App.cpp
@@ -6,6 +6,11 @@
 #include "App.hpp"
 wxIMPLEMENT_APP(App);  // entry point is in here
 
+
+// the conditional compilation here is bad and i will be changing this
+
+#ifdef __UNIX__
+
 bool App::OnInit() {
     MainFrame* mainFrame = new MainFrame("DrinkDB");
     mainFrame->SetClientSize(800,800);
@@ -16,3 +21,20 @@ bool App::OnInit() {
     
     return true;
 }
+
+#endif
+
+#ifdef _WIN32_
+
+    bool App::OnInit() {
+    MainFrame* mainFrame = new MainFrame("DrinkDB");
+    mainFrame->SetClientSize(700,700);
+    mainFrame->Center();
+    mainFrame->Show();
+    //wxImage::AddHandler(new wxPNGHandler);
+    
+    
+    return true;
+}
+
+#endif
